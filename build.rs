4c5b09e649ebc99bd19ae9701b6a2e8ffd1d1406
ChingCdesu <chingc.ccmail@gmail.com>
2023-12-16 02:29:39 +0800

    feat: support windows

--- build.rs
+++ build.rs
@@ -6,10 +6,23 @@ use std::path::PathBuf;
 use cmake::Config;
 
 fn main() {
-    let dst = Config::new(".").build_target("n2n").build();
+    let mut config = Config::new(".");
+    let target = config.build_target("n2n");
+    let dst = target.build();
+
+    let profile = config.get_profile();
+
+    let lib_dir = if env::consts::OS == "windows" { format!("build/n2n/{}", profile) } else { "build/n2n".to_string() };
+
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=n2n");
-    println!("cargo:rustc-flags=-L{}", dst.join("build/n2n").display());
+    println!("cargo:rustc-flags=-L{}", dst.join(lib_dir).display());
+
+    let mut clang_args = vec!["-DCMAKE_BUILD"];
+    if env::consts::OS == "windows" {
+        clang_args.push("-DWIN32");
+        clang_args.push("-In2n/win32");
+    }
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -17,7 +30,9 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("n2n/include/n2n.h")
         .derive_default(true)
-        .clang_args(["-DCMAKE_BUILD"])
+        .size_t_is_usize(env::consts::OS != "windows")
+        .no_default("tagMONITORINFOEXA")
+        .clang_args(clang_args)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
